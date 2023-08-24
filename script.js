7e8584d5156f648d2927d9ee7142384146b41157
Chandanpaulstu <chandanpaulstu@gmail.com>
2023-08-24 17:41:25 +0600

    8th

---
+++ script.js
@@ -0,0 +1,6 @@
+const lengthSlider=document.querySelector(".pass-length input"),
+options=document.querySelectorAll(".option input"),
+copyIcon=document.querySelector(".input-box span"),
+passwordInput=document.querySelector(".input-box input"),
+passIndicator=document.querySelector(".pass-indicator"),
+generateBtn=document.querySelector(".generate-btn");
